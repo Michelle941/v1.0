79a867d9e5e438e64a82c22204221675193b8804
Michelle Basti <michellbasti941@gmail.com>
2015-09-07 18:44:22 -0700

    About us page

---
+++ views/site/about.php
@@ -0,0 +1,114 @@
+<style>
+    p{
+        line-height: 30px;
+        margin: 15px;
+        font-family:&quot;
+        Century Gothic&quot;;
+        text-transform:uppercase;
+        margin-bottom;35px
+    }
+</style>
+<div>
+    <div lang="EN-US" link="blue" vlink="purple">
+        <div>
+            <p style="text-align: center"><img src="/images/image001.jpg"></p>
+            <p><span>Approached by the CIA to build a spaceship for exploration of Uranus, we used our top-level resources instead to invent a new way to party and make friends.</span></p>
+            <p><span >Love,</span></p>
+            <p><span >Michelle &amp; Lexi</span></p>
+            <br><br>
+
+            <p>
+                <b><u><span style="font-size:14.0pt;font-family:&quot;Century Gothic&quot;;text-transform:uppercase">How it works</span></u></b>
+                <br>
+                <span >Here are some tips to help you navigate the site.</span>
+            </p>
+            <p>
+                <b><span >Wanna <i>just </i>go to a party?</span></b>
+                <br>
+                <span >No problem. Buy a ticket and knock your socks off.</span>
+            </p>
+
+            <p>
+                <b><span >Wanna join the club?</span></b>
+                <br>
+                <span > Coo. Start by creating a profile with a few photos of yourself. Then go ham at one of our parties. The fun begins as everyone shares photos in the party albums. The more party photos, the more badass.</span>
+            </p>
+
+            <p>
+                <b><span >Spot a hottie in one of the photos?</span></b>
+                <br>
+                <span > It's your lucky day. You can message other members on the website, or, better yet, meet them in real life at the next party. </span></p>
+
+            <p>
+                <b><span >Wanna follow someone?</span></b>
+                <br>
+                <span >Go ahead.<span>  </span>We won't tell. Everyone's followers are kept secret. Why? You know why (wink <span>wink</span>).</span>
+            </p>
+
+            <br><br>
+
+            <p><b><u><span style="font-size:14.0pt;font-family:&quot;Century Gothic&quot;;text-transform:uppercase">fAQS</span></u></b>
+                <b><span style="font-size:14.0pt;font-family:&quot;Century Gothic&quot;;text-transform:uppercase"> <u></u></span></b></p>
+
+            <p>
+                <b><span >What is 941 Social Club?</span></b>
+                <br>
+                <span >941 Social Club is a community of fun mofos who share experiences and share photos. C'mon, just read our tag line.</span>
+            </p>
+
+            <p>
+                <b><span >How does membership work? </span></b>
+                <br>
+                <span >Just sign up, add photos, and party with us. </span>
+            </p>
+
+            <p><b><span >Why can I only upload 5 photos? </span></b>
+                <br>
+                <span >5 photos are all we need to know what you look like. This isn't Where's Waldo.</span>
+            </p>
+
+
+            <p>
+                <b><span >How can I make my profile private?</span></b>
+                <br>
+                <span >You can't.<span>  </span>If you don't want someone to see a photo of you, then perhaps you shouldn't put it on the internet.</span>
+            </p>
+
+            <p>
+                <b><span >Why do I need to pay to read messages from club members?</span></b>
+                <br>
+                <span >We need the $10 to stay in business. Seriously.</span>
+            </p>
+            <p>
+                <b><span >Why is this website yelling at me? </span></b>
+                <br>
+                <span >We chose to use all caps. So what? </span>
+            </p>
+
+            <p>
+                <b><span >My friend sai she is able to read messages for free. </span></b>
+                <br>
+                <span >Your friend is a liar. </span>
+            </p>
+            <p>
+                <b><span >Why do my photos keep getting shuffled around?</span></b>
+                <br>
+                <span >Chronological order is out. Shuffling is in.</span>
+            </p>
+
+            <br><br>
+
+            <p>
+                <b><u><span style="font-size:14.0pt;font-family:&quot;Century Gothic&quot;;text-transform:uppercase">contact us</span></u></b>
+                <b><span style="font-size:14.0pt;font-family:&quot;Century Gothic&quot;;text-transform:uppercase"><span>    </span></span></b>
+            </p>
+            <p><span >Want to share an idea with us?</span></p>
+
+            <p><span ><a href="mailto:holla@941socialclub.com" target="_blank">holla@941socialclub.com</a></span></p>
+
+            <br><br><br>
+
+        </div>
+
+    </div>
+</div>
