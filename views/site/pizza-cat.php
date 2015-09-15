eaa124e751f806bb7d35f4270186c2635579f541
Michelle Basti <michellbasti941@gmail.com>
2015-09-14 22:59:21 -0700

    Pizza Cat

---
+++ views/site/pizza-cat.php
@@ -0,0 +1,8 @@
+<link href="//vjs.zencdn.net/4.12/video-js.css" rel="stylesheet">
+<script src="//vjs.zencdn.net/4.12/video.js"></script>
+
+
+<video id="pizacat-video" class="video-js vjs-default-skin" loop autoplay controls preload="auto" width="1000" height="700"'>
+    <source src="/video/-Pizza_Cat_v5.mov" type='video/mp4' />
+    <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
+</video>
