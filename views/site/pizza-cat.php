d488a4842656cadeaaa3e54a357238906e1dcbe3
Michelle Basti <michellbasti941@gmail.com>
2015-09-14 23:21:54 -0700

    Pizza Cat

--- views/site/pizza-cat.php
+++ views/site/pizza-cat.php
@@ -1,12 +1,15 @@
+<style>
+    #pizacat-video{cursor: pointer;}
+    .fancybox-skin{padding: 0px !important;}
+</style>
+
 <link href="//vjs.zencdn.net/4.12/video-js.css" rel="stylesheet">
 <script src="//vjs.zencdn.net/4.12/video.js"></script>
-<video id="pizacat-video" class="video-js vjs-default-skin" loop autoplay controls preload="auto" width="1000" height="700"'>
+<video id="pizacat-video" class="video-js vjs-default-skin" loop autoplay controls preload="auto" width="100%" height="100%"'>
     <source src="/video/-Pizza_Cat_v5.mov" type='video/mp4' />
     <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
 </video>
-<style>
-    #pizacat-video{cursor: pointer;}
-</style>
+
 <script>
     $('#pizacat-video').on('click', function(){
         window.location = '/parties';
