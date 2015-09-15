09fa688306962bd143d1a6ce3da70f620717ba3d
Michelle Basti <michellbasti941@gmail.com>
2015-09-14 23:45:14 -0700

    Pizza Cat

--- web/index.php
+++ web/index.php
@@ -5,6 +5,8 @@ defined('YII_ENV') or define('YII_ENV', getenv('APPLICATION_ENV') ? getenv('APPL
 //defined('YII_DEBUG') or define('YII_DEBUG', YII_ENV !== 'prod');
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 
+ini_set('memory_limit', '-1');
+ini_set('max_execution_time', 300);
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
